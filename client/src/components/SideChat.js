7bacd963541488b4d4721322a2f3d327ea2d00bc
Kenny Vu <kennyvu@live.ca>
2020-08-21 15:37:08 -0400

    Every users now gets notified when another user joins the chat room

--- client/src/components/SideChat.js
+++ client/src/components/SideChat.js
@@ -3,30 +3,65 @@ import styled from "styled-components";
 import io from "socket.io-client";
 
 let socket;
-const BASE_URL = "localhost:8000";
+const BASE_URL = "localhost:8000"; // specify the port of the server
 
 const SideChat = () => {
   const [input, setInput] = useState("");
+  const [message, setMessage] = useState({ text: null, id: null });
+  const [feed, setFeed] = useState([]);
+
+  const userName = "Penpen 🐧"; //for now user is static
+  const room = "club Penguin"; // hard coded room for now
+
+  //On mount, user connects to socket.io and sends info of User that just joined to BE
+  useEffect(() => {
+    socket = io(BASE_URL);
+
+    socket.emit("user-joins", { userName, room });
+    socket.on("welcome", ({ text, id }) => {
+      setFeed((feed) => [...feed, { text, id }]);
+    });
+    socket.on("friend-joined", ({ text, id }) => {
+      setFeed((feed) => [...feed, { text, id }]);
+    });
+    console.log(`socket: ${socket}`);
+  }, []);
+
+  //client receives user info from backend and adds user welcome message
+
+  useEffect(() => {
+    socket.on("display-message", ({ text, id }) => {
+      setMessage({ text, id });
+      setFeed([...feed, { text, id }]);
+    });
+    console.log(message);
+    console.log(feed);
+  }, [feed]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(input);
-    socket.emit("input-send", { input });
+    console.log(`input: ${input}`);
+    socket.emit("input-send", { input, id: socket.id }); //send input to BE
+    setInput("");
   };
-
-  useEffect(() => {
-    socket = io(BASE_URL);
-    console.log(socket);
-  }, []);
-
   return (
     <Window>
       <h2>Conversations</h2>
+      <div>
+        {feed
+          ? feed.map((message) => {
+              return <div>{message.text}</div>;
+            })
+          : null}
+      </div>
       <ChatForm>
         <ChatInput
           placeholder="Type something man...."
           value={input}
           onChange={(e) => setInput(e.target.value)}
+          onKeyPress={(e) => {
+            if (e.key === "Enter") handleSubmit(e);
+          }}
         />
         <Submit type="submit" onClick={(e) => handleSubmit(e)}>
           Send
@@ -44,15 +79,17 @@ const Window = styled.div`
   right: 0;
   height: 100%;
   width: 40%;
-  border: solid;
+  box-shadow: 0 0 4px grey;
   h2 {
     text-align: center;
+    background: #4287f5;
+    color: white;
   }
 `;
 const ChatForm = styled.form`
   display: flex;
   justify-content: center;
-  border: solid;
+  box-shadow: 0 0 4px grey;
 `;
 const ChatInput = styled.textarea`
   height: 3rem;
@@ -61,7 +98,7 @@ const ChatInput = styled.textarea`
   font-size: 1.5rem;
   &&:focus {
     outline: none;
-    border: none;
+    border: #4287f5 solid;
   }
 `;
 const Submit = styled.button`
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
+
 const socketio = require("socket.io");
 const http = require("http");
 
@@ -10,15 +11,27 @@ const router = require("./router");
 
 const app = express();
 const server = http.createServer(app);
-const io = socketio(server);
+const io = socketio(server); //wraps io around server
 
 io.on("connection", (socket) => {
-  console.log("a user connected!");
-  socket.on("input-send", ({ input }) => {
-    console.log(input);
+  console.log("a user connected!"); // When the client connects The server is notified
+  socket.on("user-joins", ({ userName, room }) => {
+    socket.join(room);
+    socket.emit("welcome", { text: `Welcome ${userName}!`, id: socket.id });
+    socket.broadcast
+      .to(room)
+      .emit("friend-joined", {
+        text: `${userName} has joined!`,
+        id: socket.id,
+      });
+  });
+  socket.on("input-send", ({ input, id }) => {
+    //each socket automatically generates a random unique id
+    socket.emit("display-message", { text: input, id });
+    console.log(id);
   });
   socket.on("disconnect", () => {
-    console.log("user disconnected...");
+    console.log("user disconnected..."); //notify server when user leaves
   });
 });
 
@@ -28,6 +41,7 @@ server.listen(PORT, () => {
   console.info(`listening on http://localhost:${PORT}`);
 });
 
+//----> The usual way I prefer of setting up server. Might need this as reference in case I'll need Morgan or body parser later.
 // express()
 //   .use(function (req, res, next) {
 //     res.header(
